ee4e0a07bd3639f8633909f92480c7ba73f480bb
Subrata34 <subratamodak3491@gmail.com>
2024-06-23 10:42:01 +0600

    files

---
+++ myproject/myproject/views.py
@@ -0,0 +1,11 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    #return HttpResponse("this is new files")
+    return render(request,'home.html')
+
+def about(request):
+    #return HttpResponse("this is about")
+    return render(request,'about.html')
+    
